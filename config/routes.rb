b2888a53eb9233f50ec437ade4bfa2e3610d065b
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2015-08-24 19:10:12 +0200

    try 2 merging conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :reservations
-  root to: 'pages#home'
   resources :heros
+  root to: 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
