2bca8e2dd21b3e4fdaf5fd8bee6650607ca58546
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2015-08-24 17:37:34 +0200

    completed the validation of the three models

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,4 +1,6 @@
 class Hero < ActiveRecord::Base
   belongs_to :owner, class_name: "User", foreign_key: :user_id
   has_many :reservations
+
+  validates_presence_of :name, :description, :price, :user_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_many :reservations
   has_many :heros
+
+  validates_presence_of :name, :email, :password, :phone_number, :address
 end
