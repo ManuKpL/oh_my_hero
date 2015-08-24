87e14f7238aef0dbb5395547f9f277ff56f67765
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2015-08-24 17:48:15 +0200

    added the format validations of the user model (phone + email)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   has_many :heros
 
   validates_presence_of :name, :email, :password, :phone_number, :address
+  validates :email, format: { in: /\A\w+\.?\w+@[a-z]+\.[a-z]{2,3}\z/ }
+  validates :phone_number, format: { in: /\A(((00|\+)33)|0)\s?\d(\s?(\d){2}){4}\z/ }
 end
