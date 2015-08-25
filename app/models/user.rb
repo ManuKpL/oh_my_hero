fdb5d5633a2f3341245bc810aa41ec71a22721a9
Alice Morin <alicemorin@gmail.com>
2015-08-25 11:07:26 +0200

    added paperclip to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,13 @@ class User < ActiveRecord::Base
   validates :email, format: { with: /\A\w+\.?\w+@[a-z]+\.[a-z]{2,3}\z/ }
   validates :phone_number, format: { with: /\A(((00|\+)33)|0)\s?\d(\s?(\d){2}){4}\z/ }
 
+
+  # paperclip atributes
+  has_attached_file :picture,
+    styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :picture,
+    content_type: /\Aimage\/.*\z/
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
