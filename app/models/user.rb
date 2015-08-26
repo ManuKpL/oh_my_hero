8c1eba04b5ad4d5850abebe2a5b786435d1cd290
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2015-08-26 16:45:55 +0200

    prepared for demo

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ActiveRecord::Base
       user.password = Devise.friendly_token[0,20]  # Fake password for validation
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
-      user.picture = auth.info.image
+      # user.picture = auth.info.image
       user.token = auth.credentials.token
       user.token_expiry = Time.at(auth.credentials.expires_at)
     end
