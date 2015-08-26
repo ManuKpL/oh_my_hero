d7a425e3c6dc434fe3e73d100dee1d87729a839f
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2015-08-26 11:20:17 +0200

    added the geocoder gem + created the migration to heros

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825171050) do
+ActiveRecord::Schema.define(version: 20150826091656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20150825171050) do
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "heros", ["user_id"], name: "index_heros_on_user_id", using: :btree
